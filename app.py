0c562919f0e77c095b1a3cf13d1968d542000c5b
dimoz29 <123158855+dimoz29@users.noreply.github.com>
2023-09-27 17:17:20 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, jsonify
+import vonage
+
+app = Flask(__name__)
+
+# Replace with your Vonage API key and secret
+client = vonage.Client(key="b78ec810", secret="SkV66w8nnV1Wsi0v")
+sms = vonage.Sms(client)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        recipient_number = request.form["recipient_number"]
+        message_text = request.form["message_text"]
+
+        responseData = sms.send_message(
+            {
+                "from": "Vonage APIs",
+                "to": recipient_number,
+                "text": message_text,
+            }
+        )
+
+        if responseData["messages"][0]["status"] == "0":
+            message_status = "Message sent successfully."
+        else:
+            message_status = f"Message failed with error: {responseData['messages'][0]['error-text']}"
+
+        return render_template("index.html", message_status=message_status)
+
+    return render_template("index.html", message_status=None)
+
+if __name__ == "__main__":
+    app.run(debug=True)
